a00cb82810d4b92050087c100eefb64eb08b6a0f
Octcal <17701960+Octcal@users.noreply.github.com>
2025-02-26 16:42:25 +0000

    Woops

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    #path('books/released/', views.BooksReleasedView.as_view(), name='books-released'),
 ]
+
+
+
+
+
 
